1264f7ec2bbd49929a2242ff1307b715e81b77c8
Holly Klimowicz <hek52@cornell.edu>
2023-03-04 13:07:23 -0500

    wow it actually works

--- tdx copilot.js
+++ tdx copilot.js
@@ -4,7 +4,8 @@
 // @version      6
 // @description  TDX Copilot
 // @author       Holly Klimowicz <hek52@cornell.edu>
-// @match        https://tdx.cornell.edu/TDNext/Apps/32/Tickets/TicketDet?TicketID=*
+// @match        https://tdx.cornell.edu/TDNext/Apps/32/Tickets/TicketDet?TicketID=967797
+// @match        https://tdx.cornell.edu/TDNext/Apps/32/Tickets/Edit?TicketID=967797
 // @require      https://raw.githubusercontent.com/guetlich/code/main/support/waitForKeyElements.js
 // @require      https://raw.githubusercontent.com/guetlich/code/main/support/cookies.js
 // @require      https://raw.githubusercontent.com/guetlich/code/main/support/jquery-3.6.3.min.js
@@ -44,8 +45,6 @@ var icon_div = GM_addElement('div',
 
 var zero_span = GM_addElement(icon_div, 'span', { class: 'zeroSpan' } );
 var first_span = GM_addElement(icon_div, 'span', { class: 'firstSpan' } );
-//var $span = $( document.createElement('span') );
-//$span.addClass('folder_name').html('Elway');
 
 //add icon to the div
 var icon = GM_addElement(zero_span, 'img',
@@ -55,7 +54,6 @@ var icon = GM_addElement(zero_span, 'img',
     }
 );
 
-//var icon_i = GM_addElement(icon_div, 'img',
 var icon_i = GM_addElement(first_span, 'img',
     {
         src: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgBAMAAACBVGfHAAAABGdBTUEAALGPC/xhBQAAAA9QTFRFAAAAAAAAMDAwoKCg////aH9gHwAAAAF0Uk5TAEDm2GYAAAABYktHRACIBR1IAAAAQ0lEQVQoz2NgMEYBDAzMqAIGIAUuMOAMUmJsbAIXcIEIIPggJUQKGA8SAZchIoAR6hjxAhdxNoYJoEQuhgBGZKMnBwD7ingdHGtM+AAAAABJRU5ErkJggg==',
@@ -64,25 +62,59 @@ var icon_i = GM_addElement(first_span, 'img',
 
 
 /* where are we, and we are we doing right now? */
-var t = getCookie("copilot") ? t : "";
+//TODO: learn a better way to do this : )
+//eventually everything turns into a FSM
+//eventually everything becomes EMACS
+//eventually every app evolves to send email please shoot me if that happens
+var t = getCookie("copilot");// ? t : "";
 
 switch(t) {
     case 'turboCancel':
-        console.log("do something");
+        setCookie("copilot", "editTurboCancel", 1);
+        $('button:contains("Edit")').click();
         break;
-    case 'assignMe':
-        console.log("something else");
+    case 'editTurboCancel':
+        $('select[name="attribute40"]').val('89');
+        setCookie("copilot", "toDetail", 1);
+        $("#btnSubmit").closest('form').submit();
+        break;
+    case 'assignToMe':
+        console.log("assign to me do something");
         break;
     case 'inProcess':
-        console.log("pie");
+        console.log("in process do something");
+        setCookie("copilot", "editInProcess", 1);
+        $('button:contains("Edit")').click();
+        break; //i don't know if this ever runs nor what to do if it doesn't
+    case 'editInProcess':
+        //we are now on the edit page, and need to in process this
+        console.log("edit page in process");
+        $('select[name="attribute40"]').val('86');
+        //clear the cookie
+        setCookie("copilot", "toDetail", 1);
+        //lastly click save
+        //$('button:contains("Save")').click();
+        $("#btnSubmit").closest('form').submit();
         break;
     case 'resolve':
-        console.log("more pie");
+        setCookie("copilot", "editResolve", 1);
+        $('button:contains("Edit")').click();
+        break;
+    case 'editResolve':
+        $('select[name="attribute40"]').val('87');
+        setCookie("copilot", "toDetail", 1);
+        $("#btnSubmit").closest('form').submit();
+        break;
+    case 'toDetail':
+        setCookie("copilot", "", 0);
+        $('button:contains("To Detail")').click();
         break;
     case '':
-        console.log("nothing");
+        console.log("do nothing");
+        //fall through to default until i think of something better
         break;
     default:
+        console.log("!! destroy the cookie !!");
         setCookie("copilot", "", 0);
 }
 
@@ -90,36 +122,46 @@ switch(t) {
 
 /* remove useless Print button */
 
-//find the NoPrint button, then hide that useless thing
+//find the NoPrint button, then hide that useless boomer thing
 $('button:contains("Print View")').hide();
 
 /* add better buttons */
 
+//TODO: make this an array and instantiate repeat item creation in a loop
+//TODO: i want a pony too
+
 // TURBO CANCEL MONKEY-FIGHTER
-var thing = document.createElement ('li');
-// onclick="window.location.href = window.location.href;"
-thing.innerHTML = `<button id="turboCancel" type="button" class="btn btn-secondary btn-sm" title="TurboCancel">
+var thing0 = document.createElement ('li');
+thing0.innerHTML = `<button id="turboCancel" type="button" class="btn btn-secondary btn-sm" title="TurboCancel">
  <span class="hidden-xs padding-left-xs">Turbo Cancel</span>
  </button>`;
-$("#btnRefresh").parent().parent().append(thing);
-
-
-//find NoPrint div, then divTabHeader, the the UL in it, then append an LI
-
-
-
-// turbo cancel
-// if status in New, then allow to kill
+//$("#btnRefresh").parent().parent().append(thing0);
+$("#btnRefresh").closest('ul').append(thing0);
 
 // assign to me
-// if unassigned to anyone, assign to me
+var thing1 = document.createElement ('li');
+thing1.innerHTML = `<button id="assignToMe" type="button" class="btn btn-secondary btn-sm" title="AssignToMe">
+ <span class="hidden-xs padding-left-xs">Assign to Me</span>
+ </button>`;
+$("#btnRefresh").closest('ul').append(thing1);
 
 // in process
+var thing2 = document.createElement ('li');
+thing2.innerHTML = `<button id="inProcess" type="button" class="btn btn-secondary btn-sm" title="InProcess">
+ <span class="hidden-xs padding-left-xs">In Process</span>
+ </button>`;
+$("#btnRefresh").closest('ul').append(thing2);
 
 // resolve
+var thing3 = document.createElement ('li');
+thing3.innerHTML = `<button id="resolve" type="button" class="btn btn-secondary btn-sm" title="Resolve">
+ <span class="hidden-xs padding-left-xs">Resolve</span>
+ </button>`;
+$("#btnRefresh").closest('ul').append(thing3);
 
 /* functions */
 
+//this never gets called
 function doIt(e) {
 
     if (e.shiftKey) {
@@ -156,7 +198,7 @@ function key_down(e) {
             }
             break;
         default:
-            console.log("asdfasdf");
+            console.log("default key down block//");
 
     }
 
@@ -172,15 +214,48 @@ function key_up(e) {
     }
 }
 
+/* Event Handlers */
+
 //turbo cancel
 function turbo_cancel(e) {
-    console.log("turbo cancel");
+    console.log("entry turbo cancel");
+    setCookie("copilot", "turboCancel", 1);
+    location.reload();
 }
+
+//assign to me
+function assign_to_me(e) {
+    console.log("entry assign to me");
+    setCookie("copilot", "assignToMe", 1);
+    location.reload();
+}
+
+//resolve
+function resolve(e) {
+    console.log("entry resolve function");
+    setCookie("copilot", "resolve", 1);
+    location.reload();
+}
+
+//in process
+function in_process(e) {
+    console.log("entry in_process function");
+    setCookie("copilot", "inProcess", 1);
+    location.reload();
+}
+
 
 /* event listeners */
 
 window.addEventListener("keydown", key_down);
 window.addEventListener("keyup", key_up);
 
-document.getElementById("turboCancel").addEventListener("click", turbo_cancel);
+//clever AND janky!
+if (document.getElementById("btnRefresh")) {
+    document.getElementById("turboCancel").addEventListener("click", turbo_cancel);
+    document.getElementById("assignToMe").addEventListener("click", assign_to_me);
+    document.getElementById("inProcess").addEventListener("click", in_process);
+    document.getElementById("resolve").addEventListener("click", resolve);
+}
+
 //window.addEventListener("click", doIt);
